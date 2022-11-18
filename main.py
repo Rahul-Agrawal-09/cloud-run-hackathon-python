dd0c54a53574b75fa0bfed779478a2d0a7d23d83
rahul20108 <rahul20108@iiitd.ac.in>
2022-11-18 11:47:25 +0530

    intial

--- main.py
+++ main.py
@@ -33,9 +33,61 @@ def move():
     request.get_data()
     logger.info(request.json)
     
+    info=request.get_json()
+    self_url=info["_links"]["self"]["href"]
+    x_lim=info["arena"]["dims"][0]
+    y_lim=info["arena"]["dims"][1]
+    x_pos=info["arena"]["state"][self_url]["x"]
+    y_pos=info["arena"]["state"][self_url]["y"]
+    my_dir=info["arena"]["state"][self_url]["direction"]
+    all_x=[]
+    all_y=[]
+    x_dir=[]
+    y_dir=[]
+    for i in info["arena"]["state"].values():
+        if i["x"]==x_pos:
+            if (i["y"]-y_pos)<=3 and (i["y"]-y_pos)>=-3 and i["y"]!=y_pos:
+                #all_x.append( i["x"])
+                all_y.append(i["y"]-y_pos)
+                # y_dir.append(i["direction"])
+        elif i["y"]==y_pos:
+            if (i["x"]-x_pos<=3) and (i["x"]-x_pos)>=3 and i["x"]!=x_pos:
+                all_x.append(x_pos-i["x"])
+                #all_y.append(i["y"])
+                # x_dir.append(i["direction"])
+    # (x_dir,y_dir)=relative_direction(all_x,all_y,x_dir,y_dir)
+    for i in all_y:
+        if i<0 and my_dir=="N":
+            return moves[1]
+        elif i>0 and my_dir=="S":
+            return moves[1]
+    for i in all_x:
+        if i<0 and my_dir=="E":
+            return moves[1]
+        elif i>0 and my_dir=="W":
+            return moves[1]
+    return moves[can_move(x_pos,y_pos,x_lim-1,y_lim-1,my_dir)]
     # TODO add your implementation here to replace the random response
     
-    return moves[random.randrange(len(moves))]
+    # return moves[random.randrange(len(moves))]
+
+def can_move(x_pos,y_pos,x_lim,y_lim,dir):
+    if x_pos<=0 and dir=="W":
+        if y_pos!=0:
+            return 3
+        else:
+            return 2
+    elif x_pos>=x_lim and dir=="E":
+        if y_pos==y_lim:
+            return 2
+        else:
+            return 3
+    elif y_pos<=0 and dir=="N":
+        return 3
+    elif y_pos>=y_lim and dir=="S":
+        return 3
+    else:
+        return 0
 
 if __name__ == "__main__":
   app.run(debug=False,host='0.0.0.0',port=int(os.environ.get('PORT', 8080)))
